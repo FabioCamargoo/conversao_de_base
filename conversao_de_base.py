91c77dc33a9608d83277acf695142a8a1333a6bc
FabioCamargoo <fa.camargojr@gmail.com>
2024-05-05 17:52:00 -0300

    conversor_de_base

---
+++ conversao_de_base.py
@@ -0,0 +1,51 @@
+#o usuário coloca o número em decimal
+decimal = int(input("Coloque o número decimal aqui: "))
+#o usuário seleciona para qual base ele gostaria de converter 
+select = str(input("Para qual base você gostaria de converter: \nBinário - B \nOctadecimal - O \nHexadecimal - H: "))
+
+
+# se a resposta do usuário for B ou b, ele vai entrar nessa condição
+if select == "B" or select == "b":
+    #variável para armazenar o valor de binário
+    binario = " "
+    while decimal > 0:
+        #variável resto para pegar o resto do número decimal
+        resto = decimal % 2 
+        #variável binario transforma o resto em string e soma o valor do próprio binário
+        binario = str(resto) + binario
+        #variável decimal é dividida por ela mesmo pegando somente o valor inteiro da operação
+        decimal = decimal // 2
+    #mostra o resultado o resultado do número digitado pelo usuário na base escolhida por ele
+    print(binario)
+    # se a resposta do usuário for O ou o, ele vai entrar nessa condição
+    
+elif select == "O" or select == "o":
+    #variável para armazenar o valor de octal
+    octal = " "
+    while decimal > 0:
+        #variável resto para pegar o resto do número decimal
+        resto = decimal % 8
+        #variável octal transforma o resto em string e soma o valor do próprio octa decimal
+        octal = str(resto) + octal
+        #variável decimal é dividida por ela mesmo pegando somente o valor inteiro da operação
+        decimal = decimal // 8
+    #mostra o resultado o resultado do número digitado pelo usuário na base escolhida por ele
+    print(octal)
+    
+# se a resposta do usuário for H ou h, ele vai entrar nessa condição
+elif select == "H" or select == "h":
+     #variável para armazenar o valor de hexa
+    hexa = " "
+    while decimal > 0: 
+        #variável resto para pegar o resto do número decimal
+        resto = decimal % 16
+        #variável hexa transforma o resto em string e soma o valor do próprio octa decimal
+        hexa = str(resto) + hexa
+        #variável decimal é dividida por ela mesmo pegando somente o valor inteiro da operação
+        decimal = decimal // 16
+    #mostra o resultado o resultado do número digitado pelo usuário na base escolhida por ele
+    print(hexa)
+    
+else: 
+    #caso o usuário coloque um valor diferente da seleção
+    print("Seleção inválida")
